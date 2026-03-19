d49991d4932fa345ba406320929b0a3b75aa65b5
Chun-Bae <dbtjrdla2056@gmail.com>
2026-03-19 15:16:43 +0900

    fix: correct SSD_FE_V3 import path

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,7 @@
 from .ssd_fe_base import SSD_FE
 from .ssd_fe_v1 import SSD_FE_V1
 from .ssd_fe_v2 import SSD_FE_V2
-from .ssd_fe_v2 import SSD_FE_V3
+from .ssd_fe_v3 import SSD_FE_V3
 
 __all__ = [
     "SSD_FE",
